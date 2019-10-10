a03db55eb508218e5e43a09b6daa2740174a3a3e
Trenton <trentonmbailey@gmail.com>
2019-10-10 10:16:55 -0500

    testing

--- src/controllers/InventoryController.js
+++ src/controllers/InventoryController.js
@@ -98,6 +98,7 @@ inventoryController.route('/getFoodInventory')
 inventoryController.route('/registerCarePackage')
     .post((request, response) => {
         let data = request.body;
+        console.log(data);
         if(data !== undefined){
             let temp = new CarePackage(data.name,data.description,data.type,data.items,data.quantity,{});
             carePackageRepository.addCarePackage(temp);
